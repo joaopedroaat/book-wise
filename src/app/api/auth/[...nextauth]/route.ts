031cc6c619aa4ff670ca602136733c1dfec47a92
João Pedro <jotapedro1231@gmail.com>
2023-10-10 11:26:45 -0300

    connecting and saving user session on database

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,10 @@
 import NextAuth, { AuthOptions } from 'next-auth'
 
-import GoogleProvider from 'next-auth/providers/google'
+import { prisma } from '@/lib/prisma'
+import { PrismaAdapter } from '@auth/prisma-adapter'
+
 import GithubProvider from 'next-auth/providers/github'
+import GoogleProvider from 'next-auth/providers/google'
 
 const googleClientId = process.env.GOOGLE_CLIENT_ID
 const googleClientSecret = process.env.GOOGLE_CLIENT_SECRET
@@ -23,6 +26,7 @@ else if (!nextauthSecret)
   throw new Error('NEXTAUTH_SECRET environment variable not provided.')
 
 export const authOptions: AuthOptions = {
+  adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: googleClientId,
--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -4,5 +4,5 @@ import { authOptions } from '../api/auth/[...nextauth]/route'
 export default async function Home() {
   const session = await getServerSession(authOptions)
 
-  return <div>{session?.user?.name}</div>
+  return <div>{JSON.stringify(session)} </div>
 }
