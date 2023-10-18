e4774317cb3d6e0cc3127cc018f03d6a5d459ede
João Pedro <jotapedro1231@gmail.com>
2023-10-18 10:01:17 -0300

    remove LoginForm  external .css file

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,11 +1,10 @@
 'use client'
 
 import { useRouter } from 'next/navigation'
-import { GithubIcon } from '../GithubIcon'
-import { GoogleIcon } from '../GoogleIcon'
+import { GithubIcon } from './GithubIcon'
+import { GoogleIcon } from './GoogleIcon'
 import { signIn } from 'next-auth/react'
-import { GuestIcon } from '../GuestIcon'
-import './styles.css'
+import { GuestIcon } from './GuestIcon'
 
 export function LoginForm() {
   const router = useRouter()
@@ -17,13 +16,13 @@ export function LoginForm() {
   }
 
   return (
-    <div className="login-form-container">
-      <header>
+    <div className="w-96">
+      <header className="mb-10">
         <h1>Boas vindas!</h1>
         <p>Faça seu login ou acesse como visitante.</p>
       </header>
       <main>
-        <ul>
+        <ul className="list-none flex flex-col gap-5 p-0 [&>li]:bg-gray-600 [&>li]:flex [&>li]:items-center [&>li]:gap-5 [&>li]:py-5 [&>li]:px-6 [&>li]:rounded-lg [&>li]:cursor-pointer hover:[&>li]:bg-gray-500 [&>li]:font-bold">
           <li onClick={() => handleSignIn('google')}>
             <GoogleIcon width={24} height={20} />
             Entrar com Google
