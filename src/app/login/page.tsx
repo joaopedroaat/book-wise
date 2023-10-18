470b15530440f430b837694d3a931ca1a4d7b237
João Pedro <jotapedro1231@gmail.com>
2023-10-18 09:54:25 -0300

    remove login page external .css file

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,12 +1,11 @@
 import bookWiseLogo from '@/assets/book-wise-logo.svg'
-import Image from 'next/image'
-import './page.css'
 import { LoginForm } from '@/components/LoginForm'
+import Image from 'next/image'
 
 export default function Login() {
   return (
-    <div className="login-page-container">
-      <aside>
+    <div className="w-full h-screen p-5 grid grid-cols-5">
+      <aside className="w-full hidden col-span-2 md:flex items-center justify-center rounded-lg h-full bg-cover bg-bookwise-cover">
         <Image
           src={bookWiseLogo}
           width={183}
@@ -14,7 +13,7 @@ export default function Login() {
           alt="BookWise logo."
         />
       </aside>
-      <main>
+      <main className="col-span-full md:col-span-3 flex flex-col justify-center items-center">
         <LoginForm />
       </main>
     </div>
