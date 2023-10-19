cd7d5316471aaed6fa2cca1a7f32b750a685bea5
João Pedro <jotapedro1231@gmail.com>
2023-10-19 13:44:17 -0300

    change > to CaretRight icon

--- src/components/PopularBooksList/index.tsx
+++ src/components/PopularBooksList/index.tsx
@@ -1,6 +1,7 @@
 import { BookWiseService } from '@/services/BookWiseService'
 import Link from 'next/link'
 import { PopularBookItem } from './components/PopularBookItem'
+import { CaretRight } from '@phosphor-icons/react/dist/ssr/CaretRight'
 
 export async function PopularBooksList() {
   const popularBooks = await BookWiseService.getPopularBooks()
@@ -9,8 +10,12 @@ export async function PopularBooksList() {
     <section className="w-full">
       <header className="flex items-center justify-between mb-4">
         <h1 className="font-normal text-base">Livros populares</h1>
-        <Link className="text-purple-100 font-bold" href={'/book-wise/explore'}>
-          Ver todos {'>'}
+        <Link
+          className="text-purple-100 font-bold flex items-center gap-2"
+          href={'/book-wise/explore'}
+        >
+          Ver todos
+          <CaretRight size={16} weight="bold" />
         </Link>
       </header>
 
