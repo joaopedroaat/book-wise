fd280a7332f09dc95812a51e366c9e7f0c23c337
João Pedro <jotapedro1231@gmail.com>
2023-10-20 12:04:08 -0300

    add margin bottom on explore page h1

--- src/app/book-wise/explore/page.tsx
+++ src/app/book-wise/explore/page.tsx
@@ -4,7 +4,7 @@ import { Binoculars } from '@phosphor-icons/react/dist/ssr/index'
 export default function Explore() {
   return (
     <>
-      <h1 className="flex gap-3 text-2xl font-bold">
+      <h1 className="flex gap-3 text-2xl font-bold mb-14">
         <Binoculars className="text-green-100" size={32} />
         Explorar
       </h1>
