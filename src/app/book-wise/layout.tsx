dd034e9c25d2ddd43d8c5da78ffc9c18306956fd
João Pedro <jotapedro1231@gmail.com>
2023-11-01 09:17:01 -0300

    make menu responsive

--- src/app/book-wise/layout.tsx
+++ src/app/book-wise/layout.tsx
@@ -6,11 +6,11 @@ export default async function HomeLayout({
   children: React.ReactNode
 }) {
   return (
-    <div className="w-full h-screen p-5 grid grid-cols-12">
-      <aside className="col-span-2">
+    <div className="w-full h-screen p-5 md:grid md:grid-cols-12">
+      <aside className="bg-gray-700 border-b border-gray-600 p-3 fixed top-0 left-0 right-0 md:static md:rounded-xl md:col-span-2 md:p-6 z-50 mb-2 md:mb-0">
         <Menu />
       </aside>
-      <main className="col-span-10 px-24 pt-14 overflow-scroll">
+      <main className="col-span-12 md:col-span-10 px-24 pt-14 overflow-scroll">
         {children}
       </main>
     </div>
---
+++ src/components/Menu.tsx
@@ -0,0 +1,38 @@
+'use client'
+
+import { Binoculars, ChartLineUp, User } from '@phosphor-icons/react'
+import Link from 'next/link'
+import { BookWiseIcon } from './BookWiseIcon'
+import { usePathname } from 'next/navigation'
+
+export function Menu() {
+  const currentPage = usePathname().split('/').pop()
+
+  return (
+    <div className="flex justify-between items-center md:flex-col">
+      <BookWiseIcon />
+      <nav className="flex md:flex-col md:gap-4 gap-3 md:mt-16 text-sm md:text-lg [&>a.active]:border-green-200 [&>a]:border-transparent [&>a]:flex [&>a]:items-center [&>a]:gap-2 [&>a]:sm:gap-3 [&>a]:md:text-lg [&>a]:text-gray-400 [&>a]:no-underline hover:[&>a]:text-gray-300  [&>a.active]:text-gray-200 ">
+        <Link href="home" className={`${currentPage === 'home' && 'active'}`}>
+          <ChartLineUp size={'1.5rem'} />
+          Início
+        </Link>
+
+        <Link
+          href="explore"
+          className={`${currentPage === 'explore' && 'active'}`}
+        >
+          <Binoculars size={'1.5em'} />
+          Explorar
+        </Link>
+
+        <Link
+          href="profile"
+          className={`${currentPage === 'profile' && 'active'}`}
+        >
+          <User size={'1.5rem'} />
+          Perfil
+        </Link>
+      </nav>
+    </div>
+  )
+}
