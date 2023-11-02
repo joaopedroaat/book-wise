8d0c3dee76e9ca417a7fc358e5b244c0a79e0255
João Pedro <jotapedro1231@gmail.com>
2023-11-02 11:30:11 -0300

    change padding on small screens

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -10,7 +10,7 @@ export default async function HomeLayout({
       <aside className="bg-gray-700 border-b border-gray-600 p-3 fixed top-0 left-0 right-0 md:static md:rounded-xl md:col-span-3 lg:col-span-2 md:p-6 mb-2 md:mb-0">
         <Menu />
       </aside>
-      <main className="col-span-12 md:col-span-9 lg:col-span-10 px-24 pt-14 overflow-scroll">
+      <main className="col-span-12 md:col-span-9 lg:col-span-10 px-2 md:px-24 pt-14 overflow-scroll">
         {children}
       </main>
     </div>
