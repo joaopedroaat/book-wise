4a7ef71325ec96d7537b2c1e6b5b8db1bd980a81
João Pedro <jotapedro1231@gmail.com>
2023-11-03 18:40:55 -0300

    add styles for comment section

--- src/components/BookOverlay/components/CommentSection.tsx
+++ src/components/BookOverlay/components/CommentSection.tsx
@@ -9,17 +9,20 @@ type CommentSectionProps = {
 
 export function CommentSection({ ratings }: CommentSectionProps) {
   return (
-    <section className="mt-4">
+    <section className="mt-4 flex flex-col gap-4">
       <div className="flex items-center justify-between">
         <small>Avaliações</small>
         <button className="text-purple-100 font-bold text-sm">Avaliar</button>
       </div>
 
-      <ul>
+      <ul className="flex flex-col gap-3">
         {ratings.map((rating) => (
-          <li className="bg-gray-700 p-6 rounded-lg" key={rating.id}>
+          <li
+            className="bg-gray-700 p-6 rounded-lg flex flex-col gap-5"
+            key={rating.id}
+          >
             <div className="flex justify-between items-start">
-              <div className="flex">
+              <div className="flex gap-4">
                 <Avatar
                   user={{
                     id: rating.user.id,
@@ -28,15 +31,15 @@ export function CommentSection({ ratings }: CommentSectionProps) {
                   }}
                 />
                 <div>
-                  <p>{rating.user.name}</p>
-                  <small>
+                  <p className="font-bold text-sm">{rating.user.name}</p>
+                  <small className="text-gray-400">
                     {calculateDateDistance(new Date(rating.user.created_at))}
                   </small>
                 </div>
               </div>
               <StarRating rating={rating.rate} size={14} />
             </div>
-            {rating.description}
+            <p>{rating.description}</p>
           </li>
         ))}
       </ul>
