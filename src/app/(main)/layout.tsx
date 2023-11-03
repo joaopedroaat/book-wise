827ca1c0d3fc8c96272d8b2ddd436f4bf0463f54
João Pedro <jotapedro1231@gmail.com>
2023-11-03 13:06:14 -0300

    fix padding issue on layout

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -7,10 +7,10 @@ export default async function HomeLayout({
 }) {
   return (
     <div className="w-full h-screen p-5 md:grid md:grid-cols-12">
-      <aside className="bg-gray-700 border-b border-gray-600 p-3 fixed top-0 left-0 right-0 md:static md:rounded-xl md:col-span-3 lg:col-span-2 md:p-6 mb-2 md:mb-0">
+      <aside className="bg-gray-700 border-gray-600 border-b md:border-0 p-3 md:p-6 fixed top-0 left-0 right-0 md:static md:rounded-xl md:col-span-3 lg:col-span-2">
         <Menu />
       </aside>
-      <main className="col-span-12 md:col-span-9 lg:col-span-10 px-2 md:px-24 pt-14 overflow-scroll">
+      <main className="col-span-12 md:col-span-9 lg:col-span-10 px-2 md:px-24 pt-14 pb-5 md:pb-0 overflow-scroll">
         {children}
       </main>
     </div>
