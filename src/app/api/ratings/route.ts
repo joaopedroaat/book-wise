d030b9edad1e733230a18e3a5a054b9e86016c31
João Pedro <jotapedro1231@gmail.com>
2023-11-03 08:55:43 -0300

    add searchParams validation to /api/ratings

--- src/app/api/ratings/route.ts
+++ src/app/api/ratings/route.ts
@@ -1,17 +1,47 @@
 import { prisma } from '@/lib/prisma'
+import { z } from 'zod'
+
+const searchParamsSchema = z.object({
+  page: z
+    .number()
+    .nonnegative()
+    .nullable()
+    .transform((val) => val || 1),
+  includeUser: z
+    .enum(['true', 'false'])
+    .nullable()
+    .transform((val) => val === 'true'),
+  includeBook: z
+    .enum(['true', 'false'])
+    .nullable()
+    .transform((val) => val === 'true'),
+})
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url)
-  const page = Math.abs(Number(searchParams.get('page'))) || 1
-  const includeUsers = searchParams.get('includeUsers') === 'true'
-  const includeBooks = searchParams.get('includeBooks') === 'true'
+
+  const validatedSearchParams = searchParamsSchema.safeParse({
+    page: searchParams.get('page'),
+    includeUser: searchParams.get('includeUser'),
+    includeBook: searchParams.get('includeBook'),
+  })
+
+  if (!validatedSearchParams.success)
+    return Response.json(
+      { error: validatedSearchParams.error },
+      { status: 400 },
+    )
+
+  const { page, includeBook, includeUser } = validatedSearchParams.data
+
+  const ratingsPerPage = 10
 
   const ratings = await prisma.rating.findMany({
-    skip: (page - 1) * 10,
+    skip: (page - 1) * ratingsPerPage,
     take: 10,
     include: {
-      user: includeUsers,
-      book: includeBooks && {
+      user: includeUser,
+      book: includeBook && {
         include: {
           ratings: true,
           categories: true,
