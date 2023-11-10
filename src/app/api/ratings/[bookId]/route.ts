91d1c83440869b1667754638043a100357c7ed55
João Pedro <jotapedro1231@gmail.com>
2023-11-10 16:24:34 -0300

    order rating by created_at

--- src/app/api/ratings/[bookId]/route.ts
+++ src/app/api/ratings/[bookId]/route.ts
@@ -45,6 +45,9 @@ export async function GET(
       include: {
         user: includeUser,
       },
+      orderBy: {
+        created_at: 'desc',
+      },
     })
 
     let parsedRatings: Rating[] | RatingWithUser[] = []
