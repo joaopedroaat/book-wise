f6e592cb02ded7c9550313b7c75346942789b051
João Pedro <jotapedro1231@gmail.com>
2023-11-28 11:12:12 -0300

    add responsive styles to LastReadingSection

--- src/app/(main)/home/components/LastReadingSection.tsx
+++ src/app/(main)/home/components/LastReadingSection.tsx
@@ -39,16 +39,16 @@ export function LastReadingSection() {
           </Link>
         </header>
         <main>
-          <div className="bg-gray-600 rounded-lg py-5 px-6 flex gap-6">
+          <div className="bg-gray-600 rounded-lg py-5 px-6 flex flex-col lg:flex-row items-center lg:items-start gap-6">
             <BookOverlay book={lastReading.book} />
-            <div className="flex flex-col flex-grow">
-              <div className="flex justify-between items-start">
+            <div className="flex flex-col">
+              <div className="flex flex-col lg:flex-row lg:justify-between items-center">
                 <span className="text-gray-300">
                   {calculateDateDistance(new Date(lastReading.createdAt))}
                 </span>
                 <StarRating rate={lastReading.book} size={16} />
               </div>
-              <div className="flex flex-col mt-3">
+              <div className="flex flex-col text-center lg:text-start mt-3">
                 <h1 className="font-bold text-lg text-gray-100">
                   {lastReading.book.name}
                 </h1>
@@ -56,7 +56,7 @@ export function LastReadingSection() {
                   {lastReading.book.author}
                 </small>
               </div>
-              <p className="mt-auto text-gray-300">
+              <p className="mt-4 text-gray-300 text-center lg:text-start">
                 {lastReading.book.summary}
               </p>
             </div>
