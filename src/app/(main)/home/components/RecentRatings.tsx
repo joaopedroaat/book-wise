79837f69ec92f8a2465b72d37abc8e9f93782338
João Pedro <jotapedro1231@gmail.com>
2023-11-29 22:28:27 -0300

    add infinite scroll to RecentRatings

--- src/app/(main)/home/components/RecentRatings.tsx
+++ src/app/(main)/home/components/RecentRatings.tsx
@@ -7,18 +7,29 @@ import { useRecentRatings } from '@/services/BookWiseService/hooks/useRecentRati
 import { RatingWithBookAndUser } from '@/services/BookWiseService/types'
 import { calculateDateDistance } from '@/utils/calculateDateDistance'
 import Link from 'next/link'
+import { RefObject, forwardRef, useEffect } from 'react'
+import { useInView } from 'react-intersection-observer'
 
 export function RecentRatings() {
-  const { data: ratings } = useRecentRatings({ page: 1 })
+  const { data, fetchNextPage, hasNextPage } = useRecentRatings()
+  const { ref, entry } = useInView()
+  const ratings = data?.pages.flatMap((page) => page)
+
+  useEffect(() => {
+    if (entry?.isIntersecting && hasNextPage) fetchNextPage()
+  }, [entry?.isIntersecting, fetchNextPage, hasNextPage])
 
   return (
     <section className="flex flex-col gap-4">
       <h1 className="text-base font-normal">Avaliações mais recentes</h1>
       <ul className="list-none flex flex-col gap-3">
-        {ratings &&
-          ratings.map((rating) => (
-            <RatingItem key={rating.id} rating={rating} />
-          ))}
+        {ratings?.map((rating, index) => (
+          <RatingItem
+            key={rating.id}
+            rating={rating}
+            ref={index === ratings.length - 1 ? ref : null}
+          />
+        ))}
       </ul>
     </section>
   )
@@ -28,37 +39,45 @@ type RatingItemProps = {
   rating: RatingWithBookAndUser
 }
 
-export function RatingItem({
-  rating: { user, book, rate, description, createdAt },
-}: RatingItemProps) {
-  return (
-    <li className="bg-gray-700 p-6 rounded-lg flex flex-col gap-8">
-      <header className="flex flex-col items-center gap-3 md:flex-row md:justify-between md:items-start">
-        <div className="flex flex-col items-center gap-3 md:flex-row">
-          <Avatar user={user} />
-          <div className="flex flex-col items-center md:items-start">
-            <Link
-              className="text-gray-100 no-underline"
-              href={`profile/${user.id}`}
-            >
-              {user.name}
-            </Link>
-            <small className="text-gray-400">
-              {calculateDateDistance(new Date(createdAt))}
-            </small>
+const RatingItem = forwardRef(
+  (
+    { rating: { user, book, rate, description, createdAt } }: RatingItemProps,
+    ref,
+  ) => {
+    return (
+      <li
+        ref={ref as RefObject<HTMLLIElement>}
+        className="bg-gray-700 p-6 rounded-lg flex flex-col gap-8"
+      >
+        <header className="flex flex-col items-center gap-3 md:flex-row md:justify-between md:items-start">
+          <div className="flex flex-col items-center gap-3 md:flex-row">
+            <Avatar user={user} />
+            <div className="flex flex-col items-center md:items-start">
+              <Link
+                className="text-gray-100 no-underline"
+                href={`profile/${user.id}`}
+              >
+                {user.name}
+              </Link>
+              <small className="text-gray-400">
+                {calculateDateDistance(new Date(createdAt))}
+              </small>
+            </div>
           </div>
-        </div>
-        <StarRating rate={rate} size={16} />
-      </header>
-      <main className="flex flex-col items-center gap-3 lg:flex-row lg:gap-5 lg:items-start">
-        <BookOverlay book={book} />
+          <StarRating rate={rate} size={16} />
+        </header>
+        <main className="flex flex-col items-center gap-3 lg:flex-row lg:gap-5 lg:items-start">
+          <BookOverlay book={book} />
 
-        <div className="text-center lg:text-start">
-          <h1 className="text-xl font-bold">{book.name}</h1>
-          <small className="text-gray-400">{book.author}</small>
-          <p className="mt-5 text-gray-300">{description}</p>
-        </div>
-      </main>
-    </li>
-  )
-}
+          <div className="text-center lg:text-start">
+            <h1 className="text-xl font-bold">{book.name}</h1>
+            <small className="text-gray-400">{book.author}</small>
+            <p className="mt-5 text-gray-300">{description}</p>
+          </div>
+        </main>
+      </li>
+    )
+  },
+)
+
+RatingItem.displayName = 'RatingItem'
--- src/services/BookWiseService/hooks/useRecentRatings.ts
+++ src/services/BookWiseService/hooks/useRecentRatings.ts
@@ -1,13 +1,21 @@
-import { useQuery } from 'react-query'
+import { useInfiniteQuery } from 'react-query'
 import { BookWiseService } from '..'
 import { RatingWithBookAndUser } from '../types'
 
-export function useRecentRatings({ page } = { page: 1 }) {
-  const query = useQuery('recent_ratings', async () => {
-    return (await BookWiseService.getRatings({
-      page,
-    })) as RatingWithBookAndUser[]
-  })
+export function useRecentRatings() {
+  const query = useInfiniteQuery(
+    ['recent_ratings'],
+    async ({ pageParam = 1 }) => {
+      return (await BookWiseService.getRatings({
+        page: pageParam,
+      })) as RatingWithBookAndUser[]
+    },
+    {
+      getNextPageParam: (lastPage, pages) => {
+        return lastPage.length === 10 ? pages.length + 1 : undefined
+      },
+    },
+  )
 
   return query
 }
