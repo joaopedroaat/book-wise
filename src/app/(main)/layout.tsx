8ee9e09e1792fdfbec16af3a4a86703eac600386
João Pedro <jotapedro1231@gmail.com>
2023-12-05 15:03:45 -0300

    fix overscroll bar on chrome

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -11,7 +11,7 @@ export default async function HomeLayout({
       <aside className="bg-gray-700 border-gray-600 border-b md:border-0 p-3 md:p-6 fixed top-0 left-0 right-0 md:static md:rounded-xl md:col-span-3 lg:col-span-2">
         <Menu />
       </aside>
-      <main className="col-span-12 md:col-span-9 lg:col-span-10 px-2 md:px-24 pt-14 pb-5 md:pb-0 overflow-scroll">
+      <main className="col-span-12 md:col-span-9 lg:col-span-10 px-2 md:px-24 pt-14 pb-5 md:pb-0 overflow-y-scroll">
         <PageTitle />
         {children}
       </main>
--- src/components/BookOverlay/index.tsx
+++ src/components/BookOverlay/index.tsx
@@ -37,7 +37,7 @@ export function BookOverlay({ book, width, height }: BookOverlayProps) {
       </Dialog.Trigger>
       <Dialog.Portal>
         <Dialog.Overlay className="fixed inset-0 bg-black opacity-60" />
-        <Dialog.Content className="overflow-scroll fixed top-0 right-0 bottom-0 w-full sm:w-3/4 md:w-3/5 lg:w-1/2 xl:w-2/5 bg-gray-800 flex flex-col gap-4 p-3 sm:py-6 sm:px-12">
+        <Dialog.Content className="overflow-y-scroll fixed top-0 right-0 bottom-0 w-full sm:w-3/4 md:w-3/5 lg:w-1/2 xl:w-2/5 bg-gray-800 flex flex-col gap-4 p-3 sm:py-6 sm:px-12">
           <Dialog.Close className="self-end text-gray-400">
             <X size={24} />
           </Dialog.Close>
