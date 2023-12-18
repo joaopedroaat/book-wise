54314482c859edc1e7fae749b65ef40e27b85640
João Pedro <jotapedro1231@gmail.com>
2023-12-18 16:13:25 -0300

    fix url agaaain

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,5 +1,10 @@
+'use client'
+
 import axios from 'axios'
 
 export const localApi = axios.create({
-  baseURL: new URL('/api', window.location.origin).toString(),
+  baseURL: new URL(
+    '/api',
+    process.env.BASE_URL || 'http://localhost:3000',
+  ).toString(),
 })
