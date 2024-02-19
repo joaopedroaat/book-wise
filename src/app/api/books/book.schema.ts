aedc1995dad707d91c457b285bba819cff282a49
João Pedro <joaopedroaat@outlook.com>
2024-02-19 13:34:49 -0300

    Add book schema and fix book route

---
+++ src/app/api/books/book.schema.ts
@@ -0,0 +1,17 @@
+import { z } from 'zod'
+
+export const bookSchema = z.object({
+  id: z.string(),
+  name: z.string(),
+  author: z.string(),
+  summary: z.string(),
+  coverUrl: z.string(),
+  totalPages: z.number(),
+  createdAt: z.date().transform((date) => date.toISOString()),
+})
+
+const bookResponseSchema = z.object({
+  books: bookSchema.array(),
+})
+
+export type BookResponse = z.infer<typeof bookResponseSchema>
--- src/app/api/books/route.ts
+++ src/app/api/books/route.ts
@@ -1,92 +1,11 @@
 import { prisma } from '@/lib/prisma'
-import {
-  Genre,
-  bookWithRatingsAndCategoriesSchema,
-} from '@/services/BookWiseService/schemas'
-import { BooksResponse } from '@/services/BookWiseService/types'
-import { z } from 'zod'
+import { BookResponse, bookSchema } from './book.schema'
 
-const searchParamsSchema = z.object({
-  page: z
-    .number()
-    .nonnegative()
-    .nullable()
-    .transform((val) => val || 1),
-  perPage: z.coerce
-    .number()
-    .nonnegative()
-    .nullable()
-    .transform((val) => val || 30),
-  category: z
-    .string()
-    .refine((val) =>
-      Object.values(Genre)
-        .map((categoryName) => categoryName.toLowerCase())
-        .includes(val.toLowerCase()),
-    )
-    .nullable(),
-  orderBy: z.enum(['popular']).nullable(),
-})
-
-export async function GET(request: Request) {
+export async function GET() {
   try {
-    const { searchParams } = new URL(request.url)
+    const books = bookSchema.array().parse(await prisma.book.findMany())
 
-    const validatedSearchParams = searchParamsSchema.safeParse({
-      page: searchParams.get('page'),
-      perPage: searchParams.get('perPage'),
-      category: searchParams.get('category'),
-      includeRatings: searchParams.get('includeRatings'),
-      includeCategories: searchParams.get('includeCategories'),
-      orderBy: searchParams.get('orderBy'),
-    })
-
-    if (!validatedSearchParams.success)
-      return Response.json(
-        { error: validatedSearchParams.error },
-        { status: 400 },
-      )
-
-    const { page, category, orderBy, perPage } = validatedSearchParams.data
-
-    const books = await prisma.book.findMany({
-      skip: (page - 1) * perPage,
-      take: perPage,
-      where: category
-        ? {
-            categories: {
-              some: {
-                category: {
-                  name: category,
-                },
-              },
-            },
-          }
-        : undefined,
-      include: {
-        ratings: {
-          include: {
-            user: true,
-          },
-        },
-        categories: {
-          include: {
-            category: true,
-          },
-        },
-      },
-      orderBy: orderBy
-        ? {
-            ratings: {
-              _count: 'desc',
-            },
-          }
-        : undefined,
-    })
-
-    const parsedBooks = bookWithRatingsAndCategoriesSchema.array().parse(books)
-
-    return Response.json({ books: parsedBooks } as BooksResponse)
+    return Response.json({ books } as BookResponse)
   } catch (error) {
     return Response.json({ error }, { status: 500 })
   }
