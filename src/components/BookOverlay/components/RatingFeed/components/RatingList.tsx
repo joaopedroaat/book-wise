fe6b538f29b2239cbfa40c5344af5802c038ff41
João Pedro <joaopedroaat@outlook.com>
2024-03-06 21:32:35 -0300

    Infinite query on ratings feed

--- src/components/BookOverlay/components/RatingFeed/components/RatingList.tsx
+++ src/components/BookOverlay/components/RatingFeed/components/RatingList.tsx
@@ -1,11 +1,10 @@
 import { Avatar } from '@/components/Avatar'
 import { StarRating } from '@/components/StarRating'
 import { calculateDateDistance } from '@/utils/calculateDateDistance'
-import { CircleNotch } from '@phosphor-icons/react'
 import { useSession } from 'next-auth/react'
 import { DeleteButton } from './DeleteButton'
 import { RatingForm } from './RatingForm'
-import { useQuery } from 'react-query'
+import { useInfiniteQuery } from 'react-query'
 import { appApi } from '@/lib/axios'
 import { Rating, User } from '@prisma/client'
 import { GetRatingsResponse } from '@/app/api/ratings/route'
@@ -16,34 +15,34 @@ type RatingListProps = {
   onAbort: () => void
 }
 
+type RatingWithUser = Rating & { user: User }
+
 export function RatingList({
   bookId,
   isRatingFormVisible,
   onAbort,
 }: RatingListProps) {
-  const { data: ratings, isLoading } = useQuery(
-    ['book_ratings', bookId],
-    async () => {
+  const { data: ratings } = useInfiniteQuery({
+    queryKey: ['ratings', bookId],
+    queryFn: async ({ pageParam = 1 }) => {
       const { data } = await appApi.get<GetRatingsResponse>('/ratings', {
         params: {
           bookId,
           user: true,
           orderBy: 'date',
+          page: pageParam,
+          perPage: 5,
         },
       })
 
-      return data.ratings as (Rating & { user: User })[]
+      return data.ratings as RatingWithUser[]
     },
-  )
+    getNextPageParam: (lastPage, allPages) => {
+      if (lastPage.length) return allPages.length + 1
+    },
+  })
 
   const user = useSession().data?.user
-
-  if (isLoading)
-    return (
-      <div className="mt-12 flex justify-center">
-        <CircleNotch className="animate-spin" size={32} />
-      </div>
-    )
 
   if (!ratings) return <p>No ratings</p>
 
@@ -52,32 +51,36 @@ export function RatingList({
       {user && isRatingFormVisible && (
         <RatingForm user={user} bookId={bookId} onAbort={onAbort} />
       )}
-      {ratings.map((rating) => (
-        <li
-          className="bg-gray-700 p-6 rounded-lg flex flex-col gap-5"
-          key={rating.id}
-        >
-          <div className="flex justify-between items-start">
-            <div className="flex gap-4">
-              <Avatar user={rating.user} />
-              <div>
-                <p className="font-bold text-sm">{rating.user.name}</p>
-                <small className="text-gray-400">
-                  {calculateDateDistance(new Date(rating.createdAt))}
-                </small>
-              </div>
-            </div>
+      {ratings.pages.flatMap((page) =>
+        page.map((rating) => (
+          <>
+            <li
+              className="bg-gray-700 p-6 rounded-lg flex flex-col gap-5"
+              key={rating.id}
+            >
+              <div className="flex justify-between items-start">
+                <div className="flex gap-4">
+                  <Avatar user={rating.user} />
+                  <div>
+                    <p className="font-bold text-sm">{rating.user.name}</p>
+                    <small className="text-gray-400">
+                      {calculateDateDistance(new Date(rating.createdAt))}
+                    </small>
+                  </div>
+                </div>
 
-            <div className="flex gap-4">
-              <StarRating type="value" rate={rating.rate} size={14} />
-              {user && user.id === rating.user.id && (
-                <DeleteButton ratingId={rating.id} />
-              )}
-            </div>
-          </div>
-          <p>{rating.description}</p>
-        </li>
-      ))}
+                <div className="flex gap-4">
+                  <StarRating type="value" rate={rating.rate} size={14} />
+                  {user && user.id === rating.user.id && (
+                    <DeleteButton ratingId={rating.id} />
+                  )}
+                </div>
+              </div>
+              <p>{rating.description}</p>
+            </li>
+          </>
+        )),
+      )}
     </ul>
   )
 }
