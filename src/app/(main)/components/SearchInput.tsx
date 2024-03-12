e95db763205a88d507c46e49ee16f3b1b90a64c1
João Pedro <joaopedroaat@outlook.com>
2024-03-12 00:12:04 -0300

    Submit form when user stops typing

--- src/app/(main)/components/SearchInput.tsx
+++ src/app/(main)/components/SearchInput.tsx
@@ -1,40 +1,36 @@
 import { ExploreContext } from '@/contexts/ExploreContext'
-import { zodResolver } from '@hookform/resolvers/zod'
 import { MagnifyingGlass } from '@phosphor-icons/react'
-import { useContext } from 'react'
-import { useForm } from 'react-hook-form'
-import { z } from 'zod'
-
-const querySchema = z.object({
-  query: z.string(),
-})
-
-type QueryForm = z.infer<typeof querySchema>
+import { FormEvent, useContext, useEffect, useState } from 'react'
 
 export function SearchInput() {
   const { query, setQuery } = useContext(ExploreContext)
+  const [term, setTerm] = useState(query)
 
-  const { register, handleSubmit } = useForm<QueryForm>({
-    resolver: zodResolver(querySchema),
-    defaultValues: {
-      query,
-    },
-  })
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setQuery(term)
+    }, 500)
 
-  const onSubmit = (data: QueryForm) => {
-    setQuery(data.query)
+    return () => clearTimeout(timeoutId)
+  }, [setQuery, term])
+
+  const handleSubmit = (evt: FormEvent<HTMLFormElement>) => {
+    evt.preventDefault()
+
+    setQuery(term)
   }
 
   return (
     <form
-      onSubmit={handleSubmit(onSubmit)}
+      onSubmit={handleSubmit}
       className="flex items-center gap-2 border border-gray-500 text-gray-300 placeholder:text-gray-400 px-4 py-2 rounded-md focus-within:outline focus-within:outline-gray-500"
     >
       <input
         type="text"
         placeholder="Buscar livro ou autor"
         className="bg-gray-800 outline-none"
-        {...register('query')}
+        value={term}
+        onChange={(evt) => setTerm(evt.target.value)}
       />
       <MagnifyingGlass className="text-gray-500" size={20} />
     </form>
